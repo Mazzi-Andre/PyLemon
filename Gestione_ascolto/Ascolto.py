697a1438ed9434204efc32a662b8fa3e434967a2
Manuel <s1092794@studenti.univpm.it>
2021-05-04 12:16:55 +0200

    Commit:Classe Ascolto

---
+++ Gestione_ascolto/Ascolto.py
@@ -0,0 +1,13 @@
+class Ascolto (object):
+
+    def __init__(self):
+        super(Ascolto, self).__init__()
+
+    def ricerca(self, key):
+        print("ciao")
+
+    def ascolto_brano(self, track):
+        print("ciao")
+
+    def crea_playlist(self, playlist):
+        print("ciao")
