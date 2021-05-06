799539415c6460910be092ad18880a4beb67f0f0
GitAndreaHub <s1095511@studenti.univpm.it>
2021-05-06 18:12:09 +0200

    Eliminazione_account con richiesta di username e password prima di poter confermare l'eliminazione del proprio account.

--- Gestione_del_profilo/Model/Eliminazione_account.py
+++ Gestione_del_profilo/Model/Eliminazione_account.py
@@ -3,18 +3,32 @@ import pickle
 
 class EliminazioneAccount():
 
-    def __init__(self):
+    def __init__(self, username, password):
         super(EliminazioneAccount, self).__init__()
-
         self.username = username
-
-    def selected_profilo(registrazione):
-        if  registrazione.username == username:
-            return true
-        else:
-            return false
-
-    def rimozione_accont(self):
-        self.registrazione.lista_profili.remove(filter(selected_profilo, self))
+        self.password = password
 
 
+    def verifica(self, username, password):
+        if username == controller_andre.username #controllore_andre.username me lo passerà andre dal pickle
+                if password == controller_andre.password #controllore_andre.password me lo passerà andre da pickle
+                    verifica = True
+                    return verifica
+        verifica = False
+        return verifica
+
+    """def rimozione_accont(self, verifica):
+        def selected_profilo(self, registrazione, username):
+            if verifica is True:
+                if  registrazione.username == username:
+                    return True
+                return False
+            self.registrazione.lista_profili.remove(list(filter(selected_profilo, self)))"""""
+
+    def rimozione_accont(self, verifica):
+        if verifica is True:
+            def selected_profilo(self, registrazione, username):
+                if registrazione.username == username:
+                    return True
+                return False
+            self.registrazione.lista_profili.remove(list(filter(selected_profilo, self)))
