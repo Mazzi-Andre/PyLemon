a108b61028de39dca9dd5c6bc4e279a26b7a467c
Mazzi <s1093211@studenti.univpm.it>
2021-05-14 16:20:57 +0200

    aggiunta funzioni login e registrazione

---
+++ Login/view/vista_login.py
@@ -0,0 +1,86 @@
+from PyQt5 import QtWidgets, QtCore
+
+
+class Ui_Outsecure(object):
+    """
+    LOGIN PAGE
+    """
+
+    def setupUi(self, Outsecure):
+        Outsecure.setObjectName("Outsecure")
+        Outsecure.resize(530, 340)
+        Outsecure.setMouseTracking(True)
+        Outsecure.setStyleSheet(
+            "border-color: rgb(255, 42, 245);\n"
+            "border-color: rgb(20, 255, 55);\n"
+            "background-color: rgb(40, 39, 39);\n"
+            "")
+
+        """serve solamente per una linea in mezzo, bocciata"""
+        # self.line = QtWidgets.QFrame(Outsecure)
+        # self.line.setGeometry(QtCore.QRect(10, 80, 591, 20))
+        # self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        # self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        # self.line.setObjectName("line")
+
+        """Label del titolo"""
+        self.l_title = QtWidgets.QLabel(Outsecure)
+        self.l_title.setGeometry(QtCore.QRect(210, 30, 121, 71))
+        self.l_title.setStyleSheet("color: rgb(221, 215, 25);\n"
+                                   "font: 30pt \".SF NS Text\";")
+        self.l_title.setObjectName("l_title")
+
+        """ pulsante del submit """
+        self.btn_Submit = QtWidgets.QPushButton(Outsecure)
+        self.btn_Submit.setGeometry(QtCore.QRect(180, 200, 161, 31))
+        self.btn_Submit.setStyleSheet("color: rgb(250, 255, 255);\n"
+                                      "background-color: rgb(73, 199, 41);\n"
+                                      "border-style:outset;\n"
+                                      "border-radius:10px;\n"
+                                      "font: 14pt \"Arial\";")
+        self.btn_Submit.setObjectName("btn_Submit")
+
+        """ pulstante del nuovo utente """
+        self.btn_newuser = QtWidgets.QPushButton(Outsecure)
+        self.btn_newuser.setGeometry(QtCore.QRect(180, 240, 161, 31))
+        self.btn_newuser.setStyleSheet("color: rgb(250, 255, 255);\n"
+                                       "background-color: rgb(73, 199, 41);\n"
+                                       "border-style:outset;\n"
+                                       "border-radius:10px;\n"
+                                       "font: 14pt \"Arial\";")
+        self.btn_newuser.setObjectName("btn_newuser")
+
+        """ funzione del copyright """
+        # self.l_copyright = QtWidgets.QLabel(Outsecure)
+        # self.l_copyright.setGeometry(QtCore.QRect(150, 310, 261, 21))
+        # self.l_copyright.setStyleSheet("color: rgb(252, 0, 28);")
+        # self.l_copyright.setObjectName("l_copyright")
+
+        self.txt_username = QtWidgets.QLineEdit(Outsecure)
+        self.txt_username.setGeometry(QtCore.QRect(130, 100, 275, 31))
+        self.txt_username.setStyleSheet("background-color: rgb(207, 211, 211);\n"
+                                        "border-style:outset;\n"
+                                        "border-radius:10px;\n"
+                                        "font: 14pt \"Arial\";")
+        self.txt_username.setObjectName("txt_username")
+        self.txt_password = QtWidgets.QLineEdit(Outsecure)
+        self.txt_password.setGeometry(QtCore.QRect(130, 150, 271, 31))
+        self.txt_password.setStyleSheet("background-color: rgb(207, 211, 211);\n"
+                                        "border-style:outset;\n"
+                                        "border-radius:10px;\n"
+                                        "font: 14pt \"Arial\";")
+        self.txt_password.setObjectName("txt_password")
+
+        self.retranslateUi(Outsecure)
+        QtCore.QMetaObject.connectSlotsByName(Outsecure)
+
+    def retranslateUi(self, Outsecure):
+        _translate = QtCore.QCoreApplication.translate
+        Outsecure.setWindowTitle(_translate("Outsecure", "Form"))
+        self.l_title.setText(_translate("Outsecure",
+                                        "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; font-style:italic;\">PySound</span></p></body></html>"))
+        self.btn_Submit.setText(_translate("Outsecure", "Submit"))
+        self.btn_newuser.setText(_translate("Outsecure", "New User"))
+        # self.l_copyright.setText(_translate("Outsecure", "This software belongs to OutSecure ")) # serve per la scritta del copyright
+        self.txt_username.setPlaceholderText(_translate("Outsecure", "Enter UserName"))
+        self.txt_password.setPlaceholderText(_translate("Outsecure", "Enter Password"))
--- main.py
+++ main.py
@@ -1,29 +1,125 @@
 import sys
-from PyQt5.QtWidgets import QApplication
-
-from tkinter import *
-from tkinter import filedialog
-from pygame import *
-import pygame
-import threading
-import os
-import tkinter.ttk as ttk
-from multiprocessing import Process
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
 
 
-#---------> import delle classi <-------------
-
-from Player.lettore import MusicPlayer
-
-
-if __name__=='__main__':
-	root = Tk()
-	app= MusicPlayer(root)
-	barThread = threading.Thread(target=1, args=(1,))
-	# set thread as daemon (thread will die if parent is killed)
-	barThread.daemon=True
-	# Start thread, could also use root.after(50, barThread.start()) if desired
-	barThread.start()
-	root.mainloop()
+# ---------- We dont Touch --------------------
+from Login.controller.controller_login import Login
 
 
+class Ui_Discovery(object):
+
+    """" clesse riguardante l'applicazione """
+
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(682, 443)
+        Form.setStyleSheet("background-color: rgb(11, 11, 11);")
+        self.tableWidget = QtWidgets.QTableWidget(Form)
+        self.tableWidget.setGeometry(QtCore.QRect(140, 70, 441, 251))
+        self.tableWidget.setStyleSheet("background-color: rgb(189, 193, 193);\n"
+                                       "color: rgb(27, 28, 28);")
+        self.tableWidget.setRowCount(10)
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.setObjectName("tableWidget")
+        self.btn_submit = QtWidgets.QPushButton(Form)
+        self.btn_submit.setGeometry(QtCore.QRect(270, 340, 181, 31))
+        self.btn_submit.setStyleSheet("color: rgb(250, 255, 255);\n"
+                                      "background-color: rgb(73, 199, 41);\n"
+                                      "border-style:outset;\n"
+                                      "border-radius:10px;\n"
+                                      "font: 14pt \"Arial\";")
+        self.btn_submit.setObjectName("btn_submit")
+        self.l_title = QtWidgets.QLabel(Form)
+        self.l_title.setGeometry(QtCore.QRect(220, 20, 251, 21))
+        self.l_title.setStyleSheet("\n"
+                                   "color:White;\n"
+                                   "font: 18pt \".SF NS Text\";")
+        self.l_title.setObjectName("l_title")
+        self.btn_back = QtWidgets.QPushButton(Form)
+        self.btn_back.setGeometry(QtCore.QRect(270, 390, 181, 31))
+        self.btn_back.setStyleSheet("color: rgb(250, 255, 255);\n"
+                                    "background-color: rgb(73, 199, 41);\n"
+                                    "border-style:outset;\n"
+                                    "border-radius:10px;\n"
+                                    "font: 14pt \"Arial\";")
+        self.btn_back.setObjectName("btn_back")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.btn_submit.clicked.connect(self.load_data)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.btn_submit.setText(_translate("Form", "Submit"))
+        self.l_title.setText(_translate("Form", "Automated Host Discovery "))
+        self.btn_back.setText(_translate("Form", "Back"))
+
+
+# ----------------------------------------------
+from Registrazione.controller.controller_registrazione import Newuser
+
+
+class Discovery(QtWidgets.QWidget, Ui_Discovery):
+    switch_window = QtCore.pyqtSignal()
+
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setupUi(self)
+        self.btn_submit.clicked.connect(self.btn_submit_handler)
+        self.btn_back.clicked.connect(self.btn_back_handler)
+
+    def pop_message(self, text=""):
+        msg = QtWidgets.QMessageBox()
+        msg.setText("{}".format(text))
+        msg.exec_()
+
+    def load_data(self):
+        self.tableWidget.setItem(0, 0, QtWidgets.QTableWidgetItem(str("Sr No")))
+        self.tableWidget.setItem(0, 1, QtWidgets.QTableWidgetItem(str(" Ip Address")))
+        self.tableWidget.setItem(0, 2, QtWidgets.QTableWidgetItem(str("Mac Address")))
+        self.tableWidget.setItem(0, 3, QtWidgets.QTableWidgetItem(str("Vendor")))
+
+    def btn_submit_handler(self):
+        self.load_data()
+
+    def btn_back_handler(self):
+        self.switch_window.emit()
+
+
+class Controller:
+
+    def __init__(self):
+        pass
+
+    def show_login_page(self):
+        self.login = Login()
+        self.login.switch_window.connect(self.show_newuser_page)
+        self.login.switch_window1.connect(self.show_discovery)
+        self.login.show()
+
+    def show_newuser_page(self):
+
+        self.newuser = Newuser()
+        self.newuser.switch_window.connect(self.show_login_page)
+        self.login.close()
+        self.newuser.show()
+
+    def show_discovery(self):
+        self.discovery = Discovery()
+        self.discovery.switch_window.connect(self.show_login_page)
+        self.login.close()
+        self.discovery.show()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    controller = Controller()
+    controller.show_login_page()
+    sys.exit(app.exec_())
+
+
+
+if __name__ == "__main__":
+    main()
