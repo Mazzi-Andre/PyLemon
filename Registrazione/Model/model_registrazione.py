17cedac0d4c5b344b2d2bc0ff1bc7684246e175f
andre <s1093211@studenti.univpm.it>
2021-05-16 19:11:35 +0200

    commit 5

---
+++ Registrazione/Model/model_registrazione.py
@@ -0,0 +1,9 @@
+class ModelRegistrazione:
+
+    def __init__(self, nome, cognome, telefono, tipo, username, password):
+        self.nome= nome
+        self.cognome = cognome
+        self.telefono = telefono
+        self.tipo = tipo
+        self.username = username
+        self.password = password
