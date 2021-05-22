ba89c6c4185c6dd2e7ba4b6fa25be5ccb25fa552
andre <s1093211@studenti.univpm.it>
2021-05-22 08:36:24 +0200

    funzione Data pt.5

--- Data_Utente/control/Data_control.py
+++ Data_Utente/control/Data_control.py
@@ -45,3 +45,15 @@ class DataPick(object):
                            {'first': nome, 'last': pas})
             self.lista= cursor.fetchone()
             return self.lista
+
+    def delete_acount(self):
+        MyData = self.get_data()
+        nome = MyData[0]
+        pas = MyData[1]
+        conn = sqlite3.connect('Data.db')
+        cursor = conn.cursor()
+        self.lista = []
+
+        with conn:
+            cursor.execute("""DELETE * FROM credentials WHERE username = :first AND password = :last""",
+                           {'first': nome, 'last': pas})
