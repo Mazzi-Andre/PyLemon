3ac282ae7bad47ba09574c630031a53c39916003
Mazzi <s1093211@studenti.univpm.it>
2021-07-07 18:13:29 +0200

    commit: commenti classe vista login

--- Login/view/vista_login.py
+++ Login/view/vista_login.py
@@ -1,10 +1,10 @@
 from PyQt5 import QtWidgets, QtCore
 
 
+
 class Ui_Outsecure(object):
-    """
-    LOGIN PAGE
-    """
+
+    """ Classe della view Login  """
 
     def setupUi(self, Outsecure):
         Outsecure.setObjectName("Outsecure")
@@ -16,14 +16,16 @@ class Ui_Outsecure(object):
             "background-color: rgb(40, 39, 39);\n"
             "")
 
-        """Label del titolo"""
+        """----------Label del titolo----------"""
+
         self.l_title = QtWidgets.QLabel(Outsecure)
         self.l_title.setGeometry(QtCore.QRect(220, 30, 140, 71))
         self.l_title.setStyleSheet("color: rgb(221, 215, 25);\n"
                                    "font: 30pt \".SF NS Text\";")
         self.l_title.setObjectName("l_title")
 
-        """ pulsante del submit """
+        """----------pulsante del submit----------"""
+
         self.btn_Submit = QtWidgets.QPushButton(Outsecure)
         self.btn_Submit.setGeometry(QtCore.QRect(180, 200, 161, 31))
         self.btn_Submit.setStyleSheet("*{border: 0.5px solid rgb(221, 215, 25);\n"
@@ -32,7 +34,8 @@ class Ui_Outsecure(object):
                                        "*:hover{background: rgb(221, 215, 25);}")
         self.btn_Submit.setObjectName("btn_Submit")
 
-        """ pulstante del nuovo utente """
+        """----------pulstante del nuovo utente----------"""
+
         self.btn_newuser = QtWidgets.QPushButton(Outsecure)
         self.btn_newuser.setGeometry(QtCore.QRect(180, 240, 161, 31))
         self.btn_newuser.setStyleSheet("*{border: 0.5px solid rgb(221, 215, 25);\n"
@@ -41,6 +44,7 @@ class Ui_Outsecure(object):
                                        "*:hover{background: rgb(221, 215, 25);}")
         self.btn_newuser.setObjectName("btn_newuser")
 
+        """----------textline del nome utente----------"""
 
         self.txt_username = QtWidgets.QLineEdit(Outsecure)
         self.txt_username.setGeometry(QtCore.QRect(130, 100, 275, 31))
@@ -49,6 +53,8 @@ class Ui_Outsecure(object):
                                         "border-style:outset;\n"
                                         "border-radius:10px;\n")
         self.txt_username.setObjectName("txt_username")
+
+        """----------textline della password----------"""
 
         self.txt_password = QtWidgets.QLineEdit(Outsecure)
         self.txt_password.setGeometry(QtCore.QRect(130, 150, 271, 31))
